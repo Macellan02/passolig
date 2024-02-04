0a2e52470836de8eda08f7d0d0f4e2793a89f4db
zeynepdurmus <zeynep.durmus@spike.com.tr>
2024-02-04 15:01:25 +0300

    drawerda biraz değişiklik

--- lib/screens/drawer.dart
+++ lib/screens/drawer.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../widgets/menu_tile1.dart';
 
+
 class Drawer1 extends StatefulWidget {
   const Drawer1({super.key});
 
@@ -12,59 +13,57 @@ class Drawer1 extends StatefulWidget {
 }
 
 class _Drawer1State extends State<Drawer1> {
+
   int expandedItem = -1;
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        backgroundColor: Color.fromRGBO(115, 115, 115, 1),
-        width: 220,
+      backgroundColor: Color.fromRGBO(115, 115, 115, 1),
+      width: 220,
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              DrawerHeader(
-                  child: Container(
-                      child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  // Profile Pic
-                  Container(
-                    padding: EdgeInsets.all(3),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: const Color.fromRGBO(220, 49, 58, 1),
-                        width: 3,
+              DrawerHeader(child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    // Profile Pic
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.grey.shade200,
+                          width: 3,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/pp.jpg',),
+                        radius: 48,
                       ),
                     ),
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/pp.jpg'),
-                      radius: 48,
-                    ),
-                  ),
-                ],
-              ))),
-              // Menü parts
+                  ],
+                )
+              )),
+              // Base Parts
               Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0, vertical: 12.0),
+                padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
                 child: Text(
                   'MENÜLER',
                   style: Theme.of(context).textTheme.bodySmall,
                 ),
               ),
-
               MenuTile1(
-                title: 'Ana Sayfa',
+                title: 'Anasayfa',
                 onTap: () {},
                 icon: Icon(Icons.home, size: 18),
                 expanded: false,
               ),
-
               MenuTile1(
-                title: 'Kart',
+                title: 'Kartlar',
                 icon: Icon(Icons.credit_card, size: 18),
                 expanded: true,
                 children: [
@@ -72,61 +71,39 @@ class _Drawer1State extends State<Drawer1> {
                     title: 'Passolig Kredi Kartı',
                   ),
                   MenuTile1(
-                    title: 'Passolig Banka Kart',
+                    title: 'Passolig Banka Kartı',
                   ),
                   MenuTile1(
-                    title: 'Passolig Cüzdan',
-                  ),
-                  MenuTile1(
-                    title: 'Passolig Gold',
+                    title: 'Passolig Cüzdan Kart',
                   ),
                 ],
               ),
               MenuTile1(
                 title: 'Biletlerim',
-                onTap: () {},
-                icon: Icon(Icons.confirmation_num, size: 18),
+                icon: Icon(Icons.confirmation_number, size: 18),
                 expanded: false,
               ),
               MenuTile1(
                 title: 'Fikstür',
-                onTap: () {},
                 icon: Icon(Icons.assignment, size: 18),
-                expanded: false,
-              ),
-
-              MenuTile1(
-                title: 'Tartışmalar',
-                icon: Icon(Icons.forum, size: 18),
                 expanded: false,
               ),
               // Definition
               Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0, vertical: 12.0),
+                padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
                 child: Text(
                   'GENEL',
                   style: Theme.of(context).textTheme.bodySmall,
                 ),
               ),
               MenuTile1(
-                title: 'Sosyal Media',
-                icon: Icon(Icons.public, size: 18),
-                expanded: false,
-              ),
-              MenuTile1(
                 title: 'Ayarlar',
                 icon: Icon(Icons.settings, size: 18),
                 expanded: false,
               ),
-
-              MenuTile1(
-                title: 'Destek',
-                icon: Icon(Icons.support_agent, size: 18),
-                expanded: false,
-              ),
             ],
           ),
-        ));
+        )
+    );
   }
 }
